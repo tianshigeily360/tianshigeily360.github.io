6b322bed977c6a6c9c0a9cc4659b98e8f9109fb5
jmaking <tianshigeily360@gmail.com>
2019-07-18 17:55:40 +0800

    Site updated: 2019-07-18 17:55:38

--- echartsDemo/demo2/beijing.js
+++ echartsDemo/demo2/beijing.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>(function (root, factory) {
+(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['exports', 'echarts'], factory);
